ed25dfcb6722ab400bdfef283901583b8c036222
Ayrton <ayrton@kelda.no>
2024-09-13 02:58:40 +0200

    fix: add missing setup configuration for jest

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import { usersData } from '../../mock/usersData';
+
+const FIRST_USER = usersData[0];
 
 test('renders user list', () => {
   render(<App />);
-  expect(screen.getByText('🚀 Elon Musk')).toBeInTheDocument();
+  expect(screen.getByText(FIRST_USER.name)).toBeInTheDocument();
 });
